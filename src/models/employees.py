25bdc9ed00fc114e4046b4e9d29288fc6208397b
ysidani <daniel.hita@phonr.com>
2021-04-01 12:34:38 +0200

    feat(models): employee model simplified

--- src/models/employees.py
+++ src/models/employees.py
@@ -2,20 +2,10 @@ import datetime
 from google.appengine.ext import db
 
 class Employee(db.Model):
-    employee_id = db.IntegerProperty()
     first_name = db.StringProperty()
     last_name = db.StringProperty()
     phone = db.StringProperty()
-    email = db.StringProperty()
-    address = db.StringProperty()
-    postal_code = db.StringProperty()
-    city = db.StringProperty()
-    province = db.StringProperty()
-    country = db.StringProperty()
-    start_date = db.DateProperty()
-    ending_date = db.DateProperty()
-    current_employee = d.BooleanProperty()
-    team = db.StringProperty()
+    email = db.StringProperty()    
 
 # employee = Employee(first_name='Antonio',
 #                     last_name='Salieri')
